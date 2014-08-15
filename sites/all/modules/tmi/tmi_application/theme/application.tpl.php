54ecb69604bf4b318b7b7ccb3c6f1652a290b164
Aaron Schachter <aaronschachter@gmail.com>
2014-08-15 17:20:34 -0400

    Adds get_theme_vars function

--- sites/all/modules/tmi/tmi_application/theme/application.tpl.php
+++ sites/all/modules/tmi/tmi_application/theme/application.tpl.php
@@ -1,15 +1,17 @@
-<p><strong>Email: <?php print $account->mail; ?></strong></p>
+<div>
+  <p><strong>Email: <?php print $account->mail; ?></strong></p>
 
-<p>Status: <?php print ucfirst($application->status); ?></p>
+  <p>Status: <?php print ucfirst($application->status); ?></p>
 
-<p>Created: <?php print format_date($application->created, 'short'); ?></p>
+  <p>Created: <?php print format_date($application->created, 'short'); ?></p>
 
-<?php if ($application->submitted): ?>
-<p>Submitted: <?php print format_date($application->submitted, 'short'); ?></p>
-<?php endif; ?>
+  <?php if ($application->submitted): ?>
+  <p>Submitted: <?php print format_date($application->submitted, 'short'); ?></p>
+  <?php endif; ?>
 
-<?php if ($application->completed): ?>
-<p>Completed: <?php print format_date($application->completed, 'short'); ?></p>
-<?php endif; ?>
+  <?php if ($application->completed): ?>
+  <p>Completed: <?php print format_date($application->completed, 'short'); ?></p>
+  <?php endif; ?>
 
-<?php print $application->submission; ?>
+  <?php print $application->submission; ?>
+</div>
