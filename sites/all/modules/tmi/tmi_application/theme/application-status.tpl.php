6854ab1012ff79f151dacf0778c6238fc02c60b4
Aaron Schachter <aaronschachter@gmail.com>
2014-08-15 17:56:07 -0400

    Adds application-status.tpl.php

---
+++ sites/all/modules/tmi/tmi_application/theme/application-status.tpl.php
@@ -0,0 +1,12 @@
+<h2>Your application has been <?php print $application->status; ?>.</h2>
+
+<?php if ($recommendations): ?>
+  <h2>Recommendations</h2>
+  <?php print $recommendations; ?>
+<?php endif; ?>
+
+<h2>Request Recommendation</h2>
+<?php print render($request_form); ?>
+
+<hr />
+<?php print $application_view; ?>
