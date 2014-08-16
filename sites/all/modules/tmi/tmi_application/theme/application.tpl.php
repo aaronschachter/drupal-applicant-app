501b0dabb7b1ccff9079514cfb04fecca34d673c
Aaron Schachter <aaronschachter@gmail.com>
2014-08-16 15:57:15 -0400

    Hide details from applicants

--- sites/all/modules/tmi/tmi_application/theme/application.tpl.php
+++ sites/all/modules/tmi/tmi_application/theme/application.tpl.php
@@ -1,7 +1,12 @@
 <div>
   <p><strong><?php print $account->mail; ?></strong></p>
+
+  <?php if ($is_admin): ?>
   <p>Application (aid: <?php print $application->aid; ?>) for <a href="/node/<?php print $application->nid_apply_for; ?>/applications">Node <?php print $application->nid_apply_for; ?></a> is <?php print $application->status; ?>.</p>
+  <?php endif; ?>
+
   <p>Created <?php print format_date($application->created, 'short'); ?></p>
+
   <?php if ($application->submitted): ?>
     <div style="background: #eee;padding:20px">
       <p>Submitted <?php print format_date($application->submitted, 'short'); ?></p>
